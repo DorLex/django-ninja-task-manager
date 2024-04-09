97449522ef772e122b64715673d1bbe0d55be328
DorLex <dorozhinskiysanya@gmail.com>
2024-04-09 23:26:16 +0300

    feat(tasks): add send_many_notifications

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,6 +1,9 @@
 from time import sleep
 
 from celery import shared_task
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 @shared_task
@@ -9,3 +12,19 @@ def send_notification(message):
     print(f'<Send {message=}>')
 
     return message
+
+
+@shared_task
+def send_many_notifications():
+    offset = 0
+    chunk_size = 3
+
+    user_emails = User.objects.all()[offset:offset + chunk_size].values_list('email', flat=True)
+
+    while user_emails:
+        print(f'<Send notifications for {user_emails=}>')
+
+        offset += chunk_size
+        user_emails = User.objects.all()[offset:offset + chunk_size].values_list('email', flat=True)
+
+    return True
